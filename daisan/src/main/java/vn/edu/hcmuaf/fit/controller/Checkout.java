1a4c20185afae0f1b6d132eafbf8a74924da3592
nthduc <nguyenthaiduc0212@gmail.com>
2022-12-07 07:11:09 +0700

    Xử lí Chức năng: Thanh Toán

---
+++ daisan/src/main/java/vn/edu/hcmuaf/fit/controller/Checkout.java
@@ -0,0 +1,31 @@
+package vn.edu.hcmuaf.fit.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.*;
+
+/**
+ * Xử lí phần thanh toán
+ *
+ * @author Nguyen Thai Duc
+ * @version 1.0
+ * @since 2022-12-06
+ */
+
+@WebServlet(name = "/Checkout", urlPatterns = "/Checkout")
+public class Checkout extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+        request.getRequestDispatcher("thanhtoan.jsp").forward(request,response);
+    }
+
+}
