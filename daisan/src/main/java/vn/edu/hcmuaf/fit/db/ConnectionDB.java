6d62684ebeffa68b4bedc6bed1931b260c17daba
nthduc <nguyenthaiduc0212@gmail.com>
2022-12-29 21:57:33 +0700

    :bug: fix connect db, and add lib

--- daisan/src/main/java/vn/edu/hcmuaf/fit/db/ConnectionDB.java
+++ daisan/src/main/java/vn/edu/hcmuaf/fit/db/ConnectionDB.java
@@ -2,16 +2,46 @@ package vn.edu.hcmuaf.fit.db;
 
 import java.sql.*;
 import java.time.LocalDate;
+/**
+ * Xử lí kết nối database
+ *
+ * @author Bui Anh Tuan
+ * @version 1.0
+ * @since 2022-12-12
+ * Cập nhất kết nối database
+ *
+ * @author Nguyen Thai Duc
+ * @version 1.0
+ * @since 2022-12-29
+ */
 
 public class ConnectionDB {
-    public static Connection con;
+    // Declare a connection
+    public static Connection conn;
+    // Declare the connection URL
+    private static final String url = "jdbc:mysql://localhost:3306/";
+    private static final String dbName = "daisan";
+    private static final String unicode = "?useUnicode=true&characterEncoding=utf-8";
+    private static final String user = "root";
+    private static final String password = "";
+    static {
+        try {
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void connect() throws SQLException, ClassNotFoundException{
-        String dbName = "daisan";
-        if (con==null || con.isClosed()){
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3307/" + dbName + "?useUnicode=true&characterEncoding=utf-8","root","123456");
+        // Get a connection to the database
+        try {
+            conn = DriverManager.getConnection(url + dbName + unicode, user, password);
+            System.out.println("Kết nối Database thành công");
+        } catch(SQLException e) {
+            e.printStackTrace();
         }
+
     }
 
     public static void main(String[] args) {
